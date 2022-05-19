80dac0a2e63ff4f3fc1c7824a376a1000456bee8
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-05-19 13:42:26 +0530

    added chakraui

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "@fontsource/inter";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
