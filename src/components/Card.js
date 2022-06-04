be2494350a5db15279eaf6938ef23707ba149a7c
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-06-04 11:48:13 +0530

    fetch all publications

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,22 +18,24 @@ export default function Card(props) {
         </div>
         <Heading size="md">{publication.title}</Heading>
         <Box color="gray">
-              <Text>{publication.author}</Text>
+          <Text>{publication.author}</Text>
         </Box>
         <Text size="xs">{publication.content}</Text>
 
         <Text color="gray">Published on {publication.date}</Text>
         <Box>
-          {publication.domains == null
-            ? <div></div>
-            : publication.domains.map((domain) => (
+          {publication.domains == null ? (
+            <div></div>
+          ) : (
+            publication.domains.map((domain) => (
               <Tag size="md" variantColor="blue">
                 {domain}
               </Tag>
-            ))}
+            ))
+          )}
         </Box>
       </Stack>
-      <Box mx="auto">
+      <Box ml="auto">
         <iframe
           title="pdf"
           src={publication.url}
--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,53 +1,39 @@
+import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
 import Card from "../components/Card";
 import { Input, Box, Button, Heading } from "@chakra-ui/react";
 import { Search2Icon } from "@chakra-ui/icons";
-import { useState } from "react";
 
 export default function ExplorePage(props) {
+  const [publications, setPublications] = useState([]);
 
-  const samplePublications = [
-    {
-      title: "The Complete Guide to React Hooks",
-      issn: "12345",
-      author: "Adam",
-      domains: ["AI"],
-      date: "2013-15-23",
-      pages: "14",
-      content:
-        "The Complete Guide to React HooksThe Complete Guide to React HooksThe Complete Guide to React HooksThe Complete Guide to React HooksThe Complete Guide to React Hooks",
-      url: "https://research.google.com/pubs/archive/44678.pdf",
-    },
-    {
-      title: "Second page",
-      issn: "22122",
-      author: "Levie",
-      domains: ["ML","CV"],
-      date: "2013-15-23",
-      pages: "14",
-      content:
-        "The Complete Guide to React HooksThe Complete Guide to React HooksThe Complete Guide to React HooksThe Complete Guide to React HooksThe Complete Guide to React Hooks",
-      url: "https://research.google.com/pubs/archive/44678.pdf"
-    }
-  ];
+  async function getPubs() {
+    var response = await supabase.from("publications").select("*");
 
-  const [publications,setPublications] = useState(samplePublications);
+    response.body.map((pub) => {
+      const { publicURL } = supabase.storage
+        .from("publications")
+        .getPublicUrl(pub.issn);
+      pub.url = publicURL;
+    });
+    setPublications(response.body);
+  }
 
   async function handleSearch() {
     var query = document.getElementById("search").value;
     var finalData = [];
 
     const contents = await supabase
-      .from('publications')
+      .from("publications")
       .select()
-      .textSearch('content', query)
+      .textSearch("content", query);
 
     finalData = finalData.concat(contents.data);
 
     const titles = await supabase
-      .from('publications')
+      .from("publications")
       .select()
-      .textSearch('title', query)
+      .textSearch("title", query);
 
     finalData = finalData.concat(titles.data);
 
@@ -57,20 +43,29 @@ export default function ExplorePage(props) {
         .getPublicUrl(finalData[i].issn);
       finalData[i].url = publicURL;
     }
-    setPublications(finalData)
+    setPublications(finalData);
   }
+
+  useEffect(() => {
+    getPubs();
+  }, []);
 
   return (
     <Box m={8}>
       <Heading size="lg" my={4}>
         Explore Research Publications
       </Heading>
-      <Input placeholder="Search for any publication" width="25%" my={4} id="search" />
-      <Button mx={2} onClick={handleSearch} >
+      <Input
+        placeholder="Search for any publication"
+        width="25%"
+        my={4}
+        id="search"
+      />
+      <Button mx={2} onClick={handleSearch}>
         <Search2Icon />
       </Button>
       {publications.map((pub) => {
-        return <Card publication = {pub} />
+        return <Card publication={pub} key={pub.issn} />;
       })}
     </Box>
   );
